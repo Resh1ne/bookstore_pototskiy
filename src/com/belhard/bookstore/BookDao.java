81fbef8c7edf6d381ad53dd19f8739889cb68bcb
Daniil Pototskiy <daniilpototskiy@mail.ru>
2023-12-05 23:09:56 +0300

    Fix: deleted comments

--- src/com/belhard/bookstore/BookDao.java
+++ src/com/belhard/bookstore/BookDao.java
@@ -4,17 +4,14 @@ import java.util.List;
 
 public interface BookDao {
     Book create(Book book);
-    //INSERT INTO books (поле1, поле2) VALUES (?, ?);
 
     Book readById(Long id);
 
     List<Book> readAll();
 
     Book update(Book book);
-    //UPDATE books SET  поле1 = ?, поле2 = ? WHERE id = ?
 
     boolean delete(Long id);
-    //DELETE FROM books WHERE id = ?;
     Book findByIsbn(String isbn);
 
     List<Book> findByAuthor(String author);
